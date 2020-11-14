075142e749ec5ad0cff13edb8c41390720b7d521
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-11-14 19:26:08 +0000

    Init model

---
+++ db/6_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  djvjudu
+-- Automatically generated sql script for the service 6, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table sadas.
+--
+CREATE TABLE djvjudu.sadas (
+  asd Integer ,
+CONSTRAINT asd_PK PRIMARY KEY (asd)
+ 
+);
+
+
+
